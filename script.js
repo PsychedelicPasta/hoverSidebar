71686a7388c78c5d553835786e6997d2759eb476
Emmanuel M <70123271+PsychedelicPasta@users.noreply.github.com>
2022-01-01 17:31:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "0px";
+}
 
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
